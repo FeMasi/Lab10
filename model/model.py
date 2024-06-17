c4ee53e6aa1aeaedab59c7d1917cc1c43cbf47aa
FeMasi <federico.masiello97@gmail.com>
2024-06-17 15:48:35 +0200

    start

--- model/model.py
+++ model/model.py
@@ -6,5 +6,28 @@ from database.DAO import DAO
 class Model:
 
     def __init__(self):
-        pass
+        self._graph = nx.Graph()
+
+    def buildGraph(self, year):
+        self._countries = DAO.getAllCountries(year)
+        self._idMap = {}
+
+        for country in self._countries:
+            self._idMap[country.CCode] = country
+
+        self._graph.clear()
+        borders = DAO.getCountryPairs(self._idMap, year)
+        self._graph.add_nodes_from(self._countries)
+        #for b in borders:
+        #    self._graph.add_edge(b.c1, b.c2)
+        self._graph.add_edges_from(borders)
+
+    def getNumCompConnesse(self):
+        return nx.number_connected_components(self._graph)
+
+    def getNumConfinanti(self, v):
+        return len(list(self._graph.neighbors(v)))
+
+    def getNodes(self):
+        return list(self._graph.nodes())
 
